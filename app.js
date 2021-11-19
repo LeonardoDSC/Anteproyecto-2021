fb7611dd8629638804798b992f45db3032d950f8
LeonardoDSC <daniel521221@gmail.com>
2021-11-18 20:48:00 -0800

    Comentarios de las propiedades utilizadas
    Agregue los comentarios de las propiedades de css y js utilizadas.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const navslide= () =>{
-    const burger=document.querySelector('.burger');
+    const burger=document.querySelector('.burger'); /*Devuelve el primer elemento del documento que coincida con el selector especificado
+    en este caso burger
+    */
     const nav= document.querySelector('.apartados');
+
     const navlinks = document.querySelectorAll('.apartados li');
 
     burger.addEventListener('click',()=>{//Añadimos el evento para el click en el burger
             //Toggle nav
         nav.classList.toggle('apartados-act');
             //Links animados
-        navlinks.forEach((link, Index)=>{
+        navlinks.forEach((link, Index)=>{/*si existe lo dejamos igual y si no existe hacemos un delay diferente para cada elemento de los apartados.*/
             if(link.style.animation){
                 link.style.animation = '';
                 
